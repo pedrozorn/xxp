c261d014f06f9fa5109242d6dfed187bf90a0384
pedrozorn <itt.pedrodev53@gmail.com>
2022-12-15 22:03:47 +0900

    bootstrap導入

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <nuxt class="bg" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.bg {
+  background-image: url("assets/img/tenagabg.png");
+  background-size: cover;
+  background-position: center center;
+  width: 100%;
+  //height: 50vh;
+  height: 100%;
+  // filter: grayscale(70%);
+  filter: opacity;
+}
+</style>
