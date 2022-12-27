c2c1e244a94dc24f1209b76cc543cec708d7bb0a
pedrozorn <itt.pedrodev53@gmail.com>
2022-12-27 21:18:57 +0900

    デプロイエラー修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -76,6 +76,11 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    compact: true,
+    babel: {
+      compact: true,
+    },
+
     // hardSource: true,
   }
 }
