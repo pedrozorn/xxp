4ca04f2caf65a65cf641ca6edc71bdaee5b7308b
pedrozorn <itt.pedrodev53@gmail.com>
2022-12-28 12:25:01 +0900

    Netlifyつ

--- pages/Contact.vue
+++ pages/Contact.vue
@@ -1,29 +1,33 @@
 <template>
   <section class="contact-container">
+    <AppBackgroundHolder :title="title" />
+    <div class="flex justify-center contact-form">
     <template v-if="!finished">
-      <form name="contact" method="POST" data-netlify="true" @submit.prevent>
-        <div class="flex justify-center contact-form">
-          <v-text-field v-model="name" :error-messages="nameErrors" :counter="10" label="お名前 ※必須" required
-            @input="$v.name.$touch()" @blur="$v.name.$touch()"></v-text-field>
-          <v-text-field v-model="company" :error-messages="companyErrors" :counter="30" label="会社名"
-            @input="$v.company.$touch()" @blur="$v.company.$touch()"></v-text-field>
-          <v-text-field v-model="email" :error-messages="emailErrors" label="メールアドレス" required @input="$v.email.$touch()"
-            @blur="$v.email.$touch()"></v-text-field>
-          <v-textarea class="message-area" v-model="message" :counter="1000" label="お問合せ内容" required
-            @input="$v.message.$touch()" @blur="$v.message.$touch()"></v-textarea>
-          <v-btn class="mr-4" @click="handleSubmit">
-            submit
-          </v-btn>
-          <v-btn @click="clear">
-            clear
-          </v-btn>
-      </div>
-    </form>
+  <form class="Form" name="contact" method="POST" data-netlify="true" @submit.prevent>
+    <div class="Form-Item">
+      <p class="Form-Item-Label"><span class="Form-Item-Label-Required">必須</span>お名前</p>
+      <input type="text" class="Form-Item-Input" placeholder="例）山田太郎">
+    </div>
+    <div class="Form-Item">
+      <p class="Form-Item-Label"><span class="Form-Item-Label-Required">必須</span>電話番号</p>
+      <input type="text" class="Form-Item-Input" placeholder="例）000-0000-0000">
+    </div>
+    <div class="Form-Item">
+      <p class="Form-Item-Label"><span class="Form-Item-Label-Required">必須</span>メールアドレス</p>
+      <input type="email" class="Form-Item-Input" placeholder="例）example@gmail.com">
+    </div>
+    <div class="Form-Item">
+      <p class="Form-Item-Label isMsg"><span class="Form-Item-Label-Required">必須</span>お問い合わせ内容</p>
+      <textarea class="Form-Item-Textarea"></textarea>
+    </div>
+    <input type="submit" class="Form-Btn" value="送信する"  @click="handleSubmit">
+  </form>
     </template>
     <template v-else>
       <p v-text="'お問い合わせ頂きありがとうございました。'" />
       <p><nuxt-link to="/" v-text="'TOPへ'" /></p>
     </template>
+  </div>
   </section>
 </template>
 <script>
@@ -131,5 +135,150 @@ export default {
 .font-test {
   font-family: 'M PLUS 1 Code', sans-serif;
   font-weight: 900;
+}
+
+.Form {
+  margin-top: 80px;
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 720px;
+}
+@media screen and (max-width: 480px) {
+  .Form {
+    margin-top: 40px;
+  }
+}
+.Form-Item {
+
+  padding-top: 24px;
+  padding-bottom: 24px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+}
+@media screen and (max-width: 480px) {
+  .Form-Item {
+    padding-left: 14px;
+    padding-right: 14px;
+    padding-top: 16px;
+    padding-bottom: 16px;
+    flex-wrap: wrap;
+  }
+}
+.Form-Item:nth-child(5) {
+
+}
+.Form-Item-Label {
+  width: 100%;
+  max-width: 248px;
+  letter-spacing: 0.05em;
+  font-weight: bold;
+  font-size: 18px;
+}
+@media screen and (max-width: 480px) {
+  .Form-Item-Label {
+    max-width: inherit;
+    display: flex;
+    align-items: center;
+    font-size: 15px;
+  }
+}
+.Form-Item-Label.isMsg {
+  margin-top: 8px;
+  margin-bottom: auto;
+}
+@media screen and (max-width: 480px) {
+  .Form-Item-Label.isMsg {
+    margin-top: 0;
+  }
+}
+.Form-Item-Label-Required {
+  border-radius: 6px;
+  margin-right: 8px;
+  padding-top: 8px;
+  padding-bottom: 8px;
+  width: 48px;
+  display: inline-block;
+  text-align: center;
+  background: #545454;
+  color: #fff;
+  font-size: 14px;
+}
+@media screen and (max-width: 480px) {
+  .Form-Item-Label-Required {
+    border-radius: 4px;
+    padding-top: 4px;
+    padding-bottom: 4px;
+    width: 32px;
+    font-size: 10px;
+  }
+}
+.Form-Item-Input {
+  border: 1px solid #ddd;
+  border-radius: 6px;
+  margin-left: 40px;
+  padding-left: 1em;
+  padding-right: 1em;
+  height: 48px;
+  flex: 1;
+  width: 100%;
+  max-width: 410px;
+  background: #eaedf2;
+  font-size: 18px;
+}
+@media screen and (max-width: 480px) {
+  .Form-Item-Input {
+    margin-left: 0;
+    margin-top: 18px;
+    height: 40px;
+    flex: inherit;
+    font-size: 15px;
+  }
+}
+.Form-Item-Textarea {
+  border: 1px solid #ddd;
+  border-radius: 6px;
+  margin-left: 40px;
+  padding-left: 1em;
+  padding-right: 1em;
+  height: 216px;
+  flex: 1;
+  width: 100%;
+  max-width: 410px;
+  background: #eaedf2;
+  font-size: 18px;
+}
+@media screen and (max-width: 480px) {
+  .Form-Item-Textarea {
+    margin-top: 18px;
+    margin-left: 0;
+    height: 200px;
+    flex: inherit;
+    font-size: 15px;
+  }
+}
+.Form-Btn {
+  border-radius: 6px;
+  margin-top: 32px;
+  margin-left: auto;
+  margin-right: auto;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  width: 280px;
+  display: block;
+  letter-spacing: 0.05em;
+  background: #545454;
+  color: #fff;
+  font-weight: bold;
+  font-size: 20px;
+}
+@media screen and (max-width: 480px) {
+  .Form-Btn {
+    margin-top: 24px;
+    padding-top: 8px;
+    padding-bottom: 8px;
+    width: 160px;
+    font-size: 16px;
+  }
 }
 </style>
